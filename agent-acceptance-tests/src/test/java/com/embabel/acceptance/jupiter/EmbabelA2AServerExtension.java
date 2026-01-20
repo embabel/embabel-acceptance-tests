f7e4bf2bd20a2cd0423258e10bba7c168db075ae
alexheifetz <aheifetz@rocketmail.com>
2026-01-20 18:17:05 -0500

    Update Embabel Keys

--- agent-acceptance-tests/src/test/java/com/embabel/acceptance/jupiter/EmbabelA2AServerExtension.java
+++ agent-acceptance-tests/src/test/java/com/embabel/acceptance/jupiter/EmbabelA2AServerExtension.java
@@ -160,11 +160,11 @@ public class EmbabelA2AServerExtension implements BeforeAllCallback, AfterAllCal
 
     private Map<String, String> buildEnvironmentVariables() {
         return Map.of(
-                "OPENAI_API_KEY", getEnvironmentVariable("OPENAI_API_KEY"),
-                "ANTHROPIC_API_KEY", getEnvironmentVariable("ANTHROPIC_API_KEY"),
-                "AWS_REGION", getEnvironmentVariable("AWS_REGION"),
-                "AWS_ACCESS_KEY_ID", getEnvironmentVariable("AWS_ACCESS_KEY_ID"),
-                "AWS_SECRET_ACCESS_KEY", getEnvironmentVariable("AWS_SECRET_ACCESS_KEY")
+                "OPENAI_API_KEY", getEnvironmentVariable("EMBABEL_OI_API_KEY"),
+                "ANTHROPIC_API_KEY", getEnvironmentVariable("EMBABEL_AC_API_KEY"),
+                "AWS_REGION", getEnvironmentVariable("us-east-2"),
+                "AWS_ACCESS_KEY_ID", getEnvironmentVariable("EMBABEL_AWS_AS_KEY_ID"),
+                "AWS_SECRET_ACCESS_KEY", getEnvironmentVariable("EMBABEL_AWS_ST_AS_KEY")
                 );
     }
 
